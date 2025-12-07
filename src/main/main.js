8f30546067859c9804b118f07a44a6346a921ebc
Minnyat <lvminhnhat@gmail.com>
2025-12-07 13:57:22 +0700

    Fix missing shortcuts by adding electron-squirrel-startup

--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,10 @@
 const { app, BrowserWindow, globalShortcut, ipcMain, clipboard, shell } = require('electron');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+    app.quit();
+}
+
 const path = require('path');
 const { registerHotkey, unregisterAll } = require('./hotkey');
 const { createTray } = require('./tray');
